e6e788bf101e874d90c6c2d5b0afe37267361b13
Zyad Khalef <118022693+ZyadKH@users.noreply.github.com>
2023-07-19 23:48:02 +0200

    The number of editions is correctly dynamically calculated

--- JS/StatsCountUp.js
+++ JS/StatsCountUp.js
@@ -1,71 +1,85 @@
 // Function to format the number with the European style (dot as thousand separator, comma as decimal separator)
 function formatNumberEuropeanStyle(number) {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-  }
-  
-  // Function to animate the stats
-  function animateStats(element, targetValue, duration) {
+}
+
+// Calculate the number of years since 1994
+function getYearsSince1994() {
+    const currentYear = new Date().getFullYear();
+    return currentYear - 1994;
+}
+
+
+// Function to animate the stats
+function animateStats(element, targetValue, duration) {
     const startValue = 0;
     const increment = targetValue / (duration / 16); // Divide by 16 for smoother animation
     let currentValue = startValue;
-  
+
     const isSpecialNumber = element.classList.contains("special-number");
-  
+
     function updateStats() {
-      currentValue += increment;
-  
-      let currentFormattedValue;
-      if (isSpecialNumber) {
-        currentFormattedValue =
-          formatNumberEuropeanStyle(currentValue.toFixed(1)).replace(".", ",");
-        // Add "M" to the formatted value
-        currentFormattedValue += " M";
-      } else {
-        currentFormattedValue = formatNumberEuropeanStyle(
-          Math.floor(currentValue)
-        );
-      }
-  
-      if (currentValue >= targetValue) {
-        element.textContent = isSpecialNumber
-          ? "2,7 M"
-          : formatNumberEuropeanStyle(Math.floor(targetValue));
-      } else {
-        element.textContent = currentFormattedValue;
-        requestAnimationFrame(updateStats);
-      }
+        currentValue += increment;
+
+        let currentFormattedValue;
+        if (isSpecialNumber) {
+            currentFormattedValue =
+                formatNumberEuropeanStyle(currentValue.toFixed(1)).replace(".", ",");
+            // Add "M" to the formatted value
+            currentFormattedValue += " M";
+        } else {
+            currentFormattedValue = formatNumberEuropeanStyle(
+                Math.floor(currentValue)
+            );
+        }
+
+        if (currentValue >= targetValue) {
+            element.textContent = isSpecialNumber
+                ? "2,7 M"
+                : formatNumberEuropeanStyle(Math.floor(targetValue));
+        } else {
+            element.textContent = currentFormattedValue;
+            requestAnimationFrame(updateStats);
+        }
     }
-  
+
     requestAnimationFrame(updateStats);
-  }
-  
-  // Start the animation once when the element is in the viewport
-  function startAnimationOnceOnScroll(entries, observer) {
+}
+
+// Set up the Intersection Observer to trigger animation
+document.addEventListener("DOMContentLoaded", function () {
+    const statsElement = document.querySelector(".merged-box .stats");
+    const targetValue = getYearsSince1994();
+    animateStats(statsElement, targetValue, 2000);
+});
+
+
+// Start the animation once when the element is in the viewport
+function startAnimationOnceOnScroll(entries, observer) {
     entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        const statsElement = entry.target.querySelector(".stats");
-        const targetValue = statsElement.textContent.replace(/[^\d,.]/g, "");
-        animateStats(
-          statsElement,
-          parseFloat(targetValue.replace(",", ".")),
-          2000
-        );
-        observer.unobserve(entry.target);
-      }
+        if (entry.isIntersecting) {
+            const statsElement = entry.target.querySelector(".stats");
+            const targetValue = statsElement.textContent.replace(/[^\d,.]/g, "");
+            animateStats(
+                statsElement,
+                parseFloat(targetValue.replace(",", ".")),
+                2000
+            );
+            observer.unobserve(entry.target);
+        }
     });
-  }
-  
-  // Set up the Intersection Observer to trigger animation
-  const statsElements = document.querySelectorAll(".box");
-  const observerOptions = {
+}
+
+// Set up the Intersection Observer to trigger animation
+const statsElements = document.querySelectorAll(".box");
+const observerOptions = {
     root: null,
     rootMargin: "0px",
     threshold: 0.1,
-  };
-  
-  const statsObserver = new IntersectionObserver(
+};
+
+const statsObserver = new IntersectionObserver(
     startAnimationOnceOnScroll,
     observerOptions
-  );
-  statsElements.forEach((element) => statsObserver.observe(element));
-  
+);
+statsElements.forEach((element) => statsObserver.observe(element));
