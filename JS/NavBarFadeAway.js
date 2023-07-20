4d7e6ea2112d12ebbb6f271ea44748210b856435
Zyad Khalef <118022693+ZyadKH@users.noreply.github.com>
2023-07-20 11:02:06 +0200

    NavBar Well done

--- JS/NavBarFadeAway.js
+++ JS/NavBarFadeAway.js
@@ -1,16 +1,23 @@
+// Get the initial scroll position
+let lastScrollPosition = window.scrollY;
 
-// Wait for the DOM content to load before executing JavaScript
-document.addEventListener('DOMContentLoaded', function() {
-    const navBar = document.querySelector('.NavBar');
-  
-    function handleScroll() {
-      if (window.scrollY > 100) {
-        navBar.classList.add('fade-out');
-      } else {
-        navBar.classList.remove('fade-out');
-      }
-    }
-  
-    window.addEventListener('scroll', handleScroll);
-  });
-  
+// Function to handle scroll event
+function handleScroll() {
+  // Get the current scroll position
+  const currentScrollPosition = window.scrollY;
+
+  // Check if scrolling down
+  if (currentScrollPosition > lastScrollPosition) {
+    // Add the 'fade-out' class to the navbar
+    document.querySelector('.NavBar').classList.add('fade-out');
+  } else {
+    // Remove the 'fade-out' class from the navbar
+    document.querySelector('.NavBar').classList.remove('fade-out');
+  }
+
+  // Update the last scroll position
+  lastScrollPosition = currentScrollPosition;
+}
+
+// Attach the 'handleScroll' function to the scroll event
+window.addEventListener('scroll', handleScroll);
